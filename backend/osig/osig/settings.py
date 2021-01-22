df8a7b452e835eb5d27257ff9ee05cd5db249b3f
joeking9292 <noonanjoe92@gmail.com>
2021-01-21 21:51:14 -0800

    Adding some settings and migrating

--- backend/osig/osig/settings.py
+++ backend/osig/osig/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'osig_calendar.apps.OsigCalendarConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
