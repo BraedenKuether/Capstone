94ec031729624b42dff3d8d5e8418911284ac5d4
Bcecil <blakemadness@gmail.com>
2021-01-22 14:44:48 -0800

    added cumulative returns and volatility metrics

--- Tester.py
+++ Tester.py
@@ -1,17 +1,38 @@
 import pyEX as px
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 import Portfolio as p
 
 class Tester:
 
-    def __init__(self,P, fMaps):
+    def __init__(self,P):
         self.portfolio = P
-        self.functions = fMaps
 
-    def apply(self,Name):
-        self.functions[fName](self.portfolio)
+    def cumulativeReturns(self,weights,withPlot=True):
+        closes = [x['close'] for x in self.portfolio.assetsByTime]
+        catted = pd.concat(closes,axis=1)
+        returns = catted.pct_change()
+        returns['pdr'] = returns.dot(weights)
+        returns = (1+returns).cumprod() 
+        if withPlot:
+            returns['pdr'].plot(title="Cumulative Returns")
+            plt.show()
+        return returns
+    
+    def risk(self,weights):
+        closes = [x['close'] for x in self.portfolio.assetsByTime]
+        catted = pd.concat(closes,axis=1)
+        dailyReturns = catted.pct_change()
+        
+        cov = (dailyReturns.cov())*252 # annulised by trading days
+        
+        var = np.dot(weights.T,np.dot(cov,weights))
+        
+        std = np.sqrt(var)
+
+        return (var,std)
 
     def plotPortfolio(self,key="close"):
         plot = plt.gca()
@@ -19,18 +40,17 @@ class Tester:
         for asset in self.portfolio.assetsByTime:
             asset.plot(y=key,ax=plot)            
 
-        plot.legend(self.portfolio.symbols)
+        legend(self.portfolio.symbols)
         plot.set_title("Daily "+key)
         plt.show()
 
 
 client = px.Client(version="sandbox")
-stonks = ['vti', 'agg', 'dbc', 'vixy']
+stonks = ['nvs', 'aapl', 'msft', 'goog']
 
 p = p.Portfolio(stonks,client)
 
-ts = Tester(p,None)
+ts = Tester(p)
 
-ts.plotPortfolio("close")
-ts.plotPortfolio("fVolume")
-
+x = ts.risk(np.array([.20,.30,.30,.20]))
+print(x)
