a0385ef29d808fd8e59132aea15e479df889c644
joeking9292 <noonanjoe92@gmail.com>
2021-01-27 00:43:14 -0800

    Adding path for HTML code

--- backend/osig/home_page/views.py
+++ backend/osig/home_page/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import Context, loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the home page.")
+    return render(request, 'calendar/calendar.html')
