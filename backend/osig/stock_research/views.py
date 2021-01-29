34b36ed622f5ab1d87b69c193acc9884dd46519a
joeking9292 <noonanjoe92@gmail.com>
2021-01-28 18:10:43 -0800

    Rendering the html for the webpage

--- backend/osig/stock_research/views.py
+++ backend/osig/stock_research/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def index(request):
+    return render(request, 'stock_research/stock_research.html')
