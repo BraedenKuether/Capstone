fa997251cd43b2d7f5ed9b51d4dc832ac0ffbcbc
joeking9292 <noonanjoe92@gmail.com>
2021-01-28 18:06:37 -0800

    Added stock research app

--- backend/osig/osig/settings.py
+++ backend/osig/osig/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home_page.apps.HomePageConfig',
+    'stock_research.apps.StockResearchConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
