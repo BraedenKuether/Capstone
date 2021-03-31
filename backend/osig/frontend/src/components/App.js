4b62cf99d24169c1cf23881af29e8945c8bfd13b
Bcecil <blakemadness@gmail.com>
2021-03-30 20:32:54 -0700

    added basic graphing

--- backend/osig/frontend/src/components/App.js
+++ backend/osig/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import {ResponsiveLine} from '@nivo/line';
+import data from "./data";
 
 class App extends Component {
   constructor(props) {
@@ -33,14 +35,71 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1> My App </h1>
+      <div style={{height:'600px',width:'1200px'}}>
+       <ResponsiveLine
+        data={data}
+         margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: 'point' }}
+           yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+            yFormat=" >-.2f"
+             axisTop={null}
+              axisRight={null}
+               axisBottom={{
+                orient: 'bottom',
+                 tickSize: 5,
+                  tickPadding: 5,
+                   tickRotation: 0,
+                    legend: 'transportation',
+                     legendOffset: 36,
+                      legendPosition: 'middle'
+                       }}
+                        axisLeft={{
+                         orient: 'left',
+                          tickSize: 5,
+                           tickPadding: 5,
+                            tickRotation: 0,
+                             legend: 'count',
+                              legendOffset: -40,
+                               legendPosition: 'middle'
+                                }}
+                                 pointSize={10}
+                                  pointColor={{ theme: 'background' }}
+                                   pointBorderWidth={2}
+                                    pointBorderColor={{ from: 'serieColor' }}
+                                     pointLabelYOffset={-12}
+                                      useMesh={true}
+                                       legends={[
+                                        {
+                                         anchor: 'bottom-right',
+                                          direction: 'column',
+                                           justify: false,
+                                            translateX: 100,
+                                             translateY: 0,
+                                              itemsSpacing: 0,
+                                               itemDirection: 'left-to-right',
+                                                itemWidth: 80,
+                                                 itemHeight: 20,
+                                                  itemOpacity: 0.75,
+                                                   symbolSize: 12,
+                                                    symbolShape: 'circle',
+                                                     symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                                                      effects: [
+                                                       {
+                                                        on: 'hover',
+                                                         style: {
+                                                          itemBackground: 'rgba(0, 0, 0, .03)',
+                                                           itemOpacity: 1
+                                                            }
+                                                             }
+                                                              ]
+                                                               }
+                                                                ]}
+                                                                 />
       </div>
     );
   }
 }
 
 export default App;
-
 const container = document.getElementById("app");
 render(<App />, container);
