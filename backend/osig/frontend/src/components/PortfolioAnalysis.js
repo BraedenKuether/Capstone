e442a65519a9ec268c8aec7038b9d7813a5ebb96
lockedarmor <ashyan99@gmail.com>
2021-03-31 23:14:33 -0700

    -Added form for submitting a request with tickers on portfolio_analysis page -Changed portfolion_analysis api endpoint to be api/portfolio_analysis

--- backend/osig/frontend/src/components/PortfolioAnalysis.js
+++ backend/osig/frontend/src/components/PortfolioAnalysis.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import {ResponsiveLine} from '@nivo/line';
-import data from "./data";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 class PortfolioAnalysis extends Component {
   constructor(props) {
@@ -18,31 +11,76 @@ class PortfolioAnalysis extends Component {
       placeholder: "Loading"
     };
   }
-
-  componentDidMount() {
-    fetch("api/lead")
-      .then(response => {
-        if (response.status > 400) {
-          return this.setState(() => {
-            return { placeholder: "Something went wrong!" };
-          });
-        }
-        return response.json();
-      })
-      .then(data => {
-        this.setState(() => {
-          return {
-            data,
-            loaded: true
-          };
-        });
-      });
+  
+  submit(values) {
+    console.log(values);
+    var reqInit = {method: 'POST', body: values};
+    console.log(reqInit)
+    var req = new Request("api/portfolio_analysis", reqInit);
+    console.log(req);
+    fetch(req).then(function(response) {
+      return response.blob();
+    }).then(function(response) {
+      console.log('fetched');
+    });
   }
 
   render() {
     return (
-      <div style={{height:'600px',width:'1200px'}}>
-      <h1>Portfolio Analysis</h1>
+      <div className='temp'>
+        <h1>Portfolio Analysis</h1>
+        <Formik
+         initialValues={{ 
+          tickers: '', 
+          checked: []
+         }}
+         validate={values => {
+           const errors = {};
+           if (!values.tickers) {
+             errors.tickers = 'Required';
+           }
+           return errors;
+         }}
+        onSubmit={(values, { setSubmitting }) => {
+                 this.submit(values)
+                 setSubmitting(false);
+         }}>
+        {props => (
+          <Form onSubmit={props.handleSubmit}>
+            <div>
+              <label style={{ display: "block" }}>Tickers</label>
+              <Field
+               type="text"
+               name="tickers"
+               id="tickers"
+               placeholder="Enter your tickers separated by commas"
+               onChange={props.handleChange}
+               onBlur={props.handleBlur}
+               value={props.values.tickers}
+               />
+              <ErrorMessage
+                component="div"
+                name="tickers"
+                className="invalid-feedback"
+              />
+              <label>
+              <Field type="checkbox" name="checked" value="Predictions" />
+              Predictions
+              </label>
+              <label>
+              <Field type="checkbox" name="checked" value="Alpha" />
+              Alpha
+              </label>
+              <label>
+              <Field type="checkbox" name="checked" value="Beta" />
+              Beta
+              </label>
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )
+        }
+        </Formik>
       </div>
     );
   }
