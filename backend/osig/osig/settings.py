85d7ea167065042a37916665cbab04d1e30bb1fe
Bcecil <blakemadness@gmail.com>
2021-04-21 15:20:05 -0700

    added permissions

--- backend/osig/osig/settings.py
+++ backend/osig/osig/settings.py
@@ -75,15 +75,6 @@ TEMPLATES = [
 ]
 
 
-REST_FRAMEWORK = {
-  'DEFAULT_AUTHENTICATION_CLASSES': (
-    'rest_framework.authentication.TokenAuthentication'
-  ),
-  'DEFAULT_PERMISSION_CLASSES': (
-    'rest_framework.permissions.IsAuthenticated'
-  )
-
-}
 
 WSGI_APPLICATION = 'osig.wsgi.application'
 
--- backend/osig/portfolio_analysis/models.py
+++ backend/osig/portfolio_analysis/models.py
@@ -9,6 +9,8 @@ class AnalysisRun(models.Model):
   title = models.CharField(max_length=100)
   path = models.CharField(max_length=300)
   date = models.DateField(default=datetime.date.today)
+  class Meta:
+    permissions = [('isManager', 'Access to Run Models for Portfolio Managers')]
   
 class AnalysisRunSerializer(serializers.Serializer):
   id = serializers.ReadOnlyField()
