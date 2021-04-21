6c0d8ef7fa3b998e58fdd337493875cadc5dc6b1
joeking9292 <noonanjoe92@gmail.com>
2021-04-20 21:11:58 -0700

    Adding paths for different api feature calls

--- backend/osig/stock_research/urls.py
+++ backend/osig/stock_research/urls.py
@@ -6,5 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='stock_research'),
+    path('income_statement/<str:ticker>', views.income_statement),
+    path('balance_sheet/<str:ticker>', views.balance_sheet),
+    path('cash_flows/<str:ticker>', views.cash_flows),
+    path('financials/<str:ticker>', views.financials)
 ] 
 #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
