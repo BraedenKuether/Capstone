e74102999eba3e66c1f46500cb089c21a9db5e42
joeking9292 <noonanjoe92@gmail.com>
2021-04-20 22:14:39 -0700

    Adding date and ticker user inputs

--- backend/osig/stock_research/forms.py
+++ backend/osig/stock_research/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms.widgets import SelectDateWidget
+from django.utils.translation import ugettext_lazy as _
 
 from time import gmtime, strftime
 
@@ -12,9 +13,13 @@ class stock_form(forms.Form):
         (current_year-3)
     ]
 
-    ticker = forms.CharField(label='Stock Ticker', max_length=5)
-    display_date = forms.DateField(
-        widget=SelectDateWidget(
-            empty_label=("Choose Year", "Choose Month", "Choose Day"),
-        ),
+    ticker = forms.RegexField(label=_("Stock Ticker"), max_length=5,
+        regex=r'^[A-Za-z0-9]+$',
+        help_text=_("5 characters or fewer is required"),
+        error_messages={'required': _("Entry only accepts letters"),}
+    )
+    display_date = forms.DateField(label=_("Year"),
+        widget=SelectDateWidget(years=YEAR_CHOICES),
+        help_text=_("Select a year"),
+        error_messages={'required': _("Need to enter a year"),}
     )
