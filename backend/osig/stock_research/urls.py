4429954cf20af1c4cae557d3a8b3558bdf8725b3
joeking9292 <noonanjoe92@gmail.com>
2021-05-12 19:54:57 -0700

    Adding url for excel workbook link

--- backend/osig/stock_research/urls.py
+++ backend/osig/stock_research/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='stock_research'),
     path('ticker_submit', views.ticker_submit, name='ticker_submit'),
+    path('excel_workbook', views.excel_workbook, name='excel_workbook'),
 ] 
 #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
