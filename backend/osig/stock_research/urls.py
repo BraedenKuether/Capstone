46c80228024ad76b874b9f5c0864226776a753a4
joeking9292 <noonanjoe92@gmail.com>
2021-05-13 20:30:48 -0700

    Adding media root

--- backend/osig/stock_research/urls.py
+++ backend/osig/stock_research/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', views.index, name='stock_research'),
     path('ticker_submit', views.ticker_submit, name='ticker_submit'),
     path('excel_workbook', views.excel_workbook, name='excel_workbook'),
-] 
-#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
