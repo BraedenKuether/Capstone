5e161d30623a2854cfe71d08812fdd20d166b8bd
lockedarmor <ashyan99@gmail.com>
2021-05-13 23:43:12 -0700

    -Fixed some routing issues as a result of merging stock reserach page

--- backend/osig/frontend/urls.py
+++ backend/osig/frontend/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='portfolio_analysis'),
     path('view_run/<str:id>/', views.view_run),
     path('', views.index ),
 ]
--- backend/osig/portfolio_analysis/urls.py
+++ backend/osig/portfolio_analysis/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('', views.index, name='api/portfolio_analysis'),
   path('get_runs/<str:id>', views.get_run),
   path('create_run',views.create_run),
  ]
